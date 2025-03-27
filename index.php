ea523f5f49455b5efce52007bdff3749cff9639b
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2025-03-27 17:20:26 -0300

    Correccion Marzo 2025

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 
 <html lang="es">
-<Head>
-<TITLE>Taller y clases de Tango en el club Juventud Unida de Ciudadela</TITLE>
+<Head> 
+<TITLE>Taller y clases de Tango</TITLE>
 
 <meta http-equiv="Content-Type" content="text/html" />
 <meta charset="utf-8" /> 
@@ -10,37 +10,35 @@
 <style>
 	* {font-family: Garamond, serif;color:dimgray;font-size:16px;
 		margin:0;padding:0;box-sizing: border-box;}
-
-	body {} /*va a ser el contenedor principal*/
-	div.encabezado{width:100%;height:200px;background-image: url("./imagenes/fluido02.jpg");background-repeat: no-repeat;background-size: cover; background-position: center}
-	div.contenedor1 {height:100%;width:100%;padding:2vw} /*Contenedor mas externo*/
+	html{height:100%;}
+	body {height:100%} /*va a ser el contenedor principal*/
+	div.encabezado{width:100%;height:20%;background-image: url("./imagenes/fluido02.jpg");
+		background-repeat: no-repeat;background-size: cover; background-position: center} /*Contenedor fijo*/
+	div.contenedor1 {height:80%;width:100%;padding: 20px;overflow:scroll} /*Contenedor scroleable*/
 	h1 {font-size:6em;text-align: center; }
-	h2 {font-size:3em;text-align: left;}
+	h2 {font-size:3em;text-align: center;}
 	h3 {font-size:2em;text-align: left;}
 	.font24{font-size:24px} /*Para aplicar a un span por ejemplo*/
-	img {
-		/*
-	    max-width: 100%; //Ancho libre permitiendo al navegador calcularla en base o otros paramentros sin sobrepasar en ancho el tamaño de la caja
+	img {	
+	    max-width: 100%; /*Ancho libre permitiendo al navegador calcularla en base o otros paramentros 
+		sin sobrepasar en ancho el tamaño de la caja*/
     	max-height: 100%;  
-		margin-bottom:1vh; //Para que cuando en dispositivos moviles se apilen, no se peguen unas con otras
-		*/
 	}
 
 
-	div.al50 {width:50%;/*background-color:lightblue*/;float:left;padding: 2vw} /*va a ser un contenedor*/
-	div.al40 {width:40%;/*background-color:lightblue*/;float:left;padding: 2vw} /*va a ser un contenedor*/
-	div.al60 {width:60%;/*background-color:gainsboro*/;float:left;padding: 2vw} /*va a ser un contenedor*/
-	div.logosImg {clear:both;height:5vh;margin-bottom:2vh;/*background-color:lightblue*/}
-	div.logosTexto{height:5vh;float:left;margin-bottom:2vh;font-size:2em/*background-color: lightblue*/;
-		display:flex;align-items: center}
-	
 	div.aparte{clear:both;height:18px;/*background-color:blue*/}
 
-	div.enlace{clear:both;height:5vh;}
-	div.enlace a{font-size: 2em}
-	div.contenedorImagen{width:33%;height:180px;border-style:none;float:left}
+	div.enlace{clear:both;height:20px;}
+	div.enlace a {font-size:2em}
+	div.contenedorPorTercio{width:33%;height:180px;/*border-style:solid*/;float:left;display:flex;align-items:center}
+	div.contenedorPorMedio{width:50%;height:180px;/*border-style:solid*/;float:left;display:flex;align-items:center}
+	div.contenedorPorDosTercios{width:66%;height:180px;/*border-style:solid*/;float:left;display:flex;align-items:center}
+	
+	div.contenedorPorTercioBajo{width:33%;height:50px;/*border-style:solid*/;float:left;display:flex;align-items:center}
+	div.contenedorPorMedioBajo{width:50%;height:50px;/*border-style:solid*/;float:left;display:flex;align-items:center}
+	div.contenedorPorDosTerciosBajo{width:66%;height:50px;/*border-style:solid*/;float:left;display:flex;align-items:center}
 
-	img.imagenBiblioteca{max-width:100%;max-height:100%}
+
 	div.bloque{display:block}
 </style>
 </Head>
@@ -49,133 +47,145 @@
 	<div class="encabezado"><h1> Tango </h1></div>
 
 	<div class="contenedor1">
-	<div class="aparte"></div>
-	<h2>Aprendé a bailar en serio</span></h2>
-	
+		<div class="aparte"></div>
+		<h2>Aprendé a bailar en serio</span></h2>
+		
 
-	<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
 
-	<div class="aparte"></div>
-
-	<hr />
-
-	<h2>Aprendé <span class="font24">a dominar tu cuerpo, a escuchar la música, a seguirla y sobre todo a hacer sentir bien a tu pareja.</span></h2>
-
-	<div class="bloque">
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/baileTango03jpeg.jpeg" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/paso02.jpeg" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/post01.jpg" />
-	</div>
-	</div>
+		<hr />
 
 
 
-	<h2>Aprendé <span class="font24">a pararte bien, a caminar y a desplazarte correctamente y nota que no solo para el baile te será útil.</span></h2>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<h2>Aprendé <span class="font24">a dominar tu cuerpo,<br/> a escuchar la música, 
+			</br>a seguirla <br/>y sobre todo <br/>a hacer sentir bien a tu pareja.</span></h2>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		
+		<div class="bloque">
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/baileTango03jpeg.jpeg" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/paso02.jpeg" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/post01.jpg" />
+			</div>
+		</div>
 
-	<div class="aparte"></div>
-	<div class="bloque">
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/post02.jpg" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/post03.png" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/post04.jpeg" />
-	</div>
-	</div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
 
-	<h2>Aprendé <span class="font24"> a marcar un abrazo firme y a la vez flexible.</span></h2>
+		<h2>Aprendé <span class="font24">a pararte bien, <br/>
+							a caminar y a desplazarte correctamente,<br/>
+							a pivotear en giro con tus metatarsos y cadera,<br/>
+							a disociar el movimiento de tu torso.<br/>
+							Todo te ayudara a flexibilizar tu cuerpo.</span></h2>
 
-	<div class="aparte"></div>
-	<div class="bloque">
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/abrazo01.png" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/abrazo02.jpeg" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/abrazo03.jpeg" />
-	</div>
-	</div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
 
-	
+		<div class="bloque">
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/post02.jpg" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/parejaTango1.jpeg" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/post04.jpeg" />
+			</div>
+		</div>
 
-	<h2>Salí a bailar<span class="font24"> pensando que tu baile no es ni mejor ni peor que el de los demás. Es diferente, especial y tal vez único.</span></h2>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+
+		<h2>Aprendé <span class="font24"> a marcar un abrazo firme <br/>y a la vez flexible.</span></h2>
+
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		
+		
+		<div class="bloque">
+			<div class="contenedorPorTercio">
+				<img  src="./imagenes/abrazo01.png" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/unico04.png" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/abrazo03.jpeg" />
+			</div>
+		</div>
+
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+
+		<h2>Salí a bailar<span class="font24"> <br/>pensando que tu baile<br/> no es ni mejor ni peor que el de los demás. 
+			<br/>Es diferente, especial <br/>y tal vez único.</span></h2>
+
+		<div class="aparte"></div>
+		<div class="aparte"></div>		
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="bloque">
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/unico01.png" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/parejaTango01.png" />
+			</div>
+			<div class="contenedorPorTercio">
+				<img src="./imagenes/unico03.png" />
+			</div>
+		</div>
+
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+
+		<div style="display:flex;background-color:#4bdeeb">
+			<div class="contenedorPorTercioBajo" >
+				<div class="contenedorPorTercioBajo" >
+					<img src="./imagenes/wahtsapp01.png" />
+				</div>
+				<div class="contenedorPorDosTerciosBajo" >
+					&nbsp;15 65069142 (Gustavo)
+				</div>
+			</div>
+			
+			<div class="contenedorPorTercioBajo" >
+				<div class="contenedorPorTercioBajo" >
+					<img src="./imagenes/gmail01.png" />
+				</div>
+				<div class="contenedorPorDosTerciosBajo">
+					&nbsp;gwittbeckertango@gmail.com
+				</div>
+			</div>
+
+		</div>
+
+		<!--<div class="contenedorPorMedio"><img src="./imagenes/instagram01.jpg" /></div>
+		<div class="logosTexto">&nbsp;@gustavowittbecker</div>-->
 
 
-	<div class="bloque">
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/unico01.png" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/unico02.png" />
-	</div>
-	<div class="contenedorImagen">
-		<img class="imagenBiblioteca" src="./imagenes/unico03.png" />
-	</div>
-	</div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
+		<div class="aparte"></div>
 
-	<div class="aparte"></div>
-<!--
-	
-
-
-	<h2></h2>
-	<img src="" />
-	<img src="" />
-	<img src="" />
-	<img src="./imagenes/unico04.png" />
-
-	<div class="aparte"></div>
-	<hr />
-
-	<div class="aparte"></div>
-
-	<div class="al40">
-	<img src="./imagenes/baileTango01.jpg">
-	</div>
-	
-	
-
-	<div class="al60">
-
--->
-	<div class="logosImg"><img src="./imagenes/wahtsapp01.png" /></div><div class="logosTexto">&nbsp;15 65069142 (Gustavo)</div>
-	
-
-	<div class="logosImg"><img src="./imagenes/gmail01.png" /></div><div class="logosTexto">&nbsp;gwittbeckertango@gmail.com</div>
-
-
-	<div class="logosImg"><img src="./imagenes/instagram01.jpg" /></div><div class="logosTexto">&nbsp;@gustavowittbecker</div>
-
-
-	<div class="logosImg"><img src="./imagenes/telefonoFijo.png" /></div>
-	<!--<div class="logosTexto">&nbsp;011 2053-9630</div>-->
-
-	<div class="aparte"></div>
-
-	<h3 id="diaHora"></h3>
-
-
-	<div class="aparte"></div>
-	<div class="aparte"></div>
-
-
-	<div id="domicilio" class="logosTexto"></div>
-	
-	
-	</div>
-
-	<div class="aparte"></div>
-
-	<!--<img style="max-width:50%" src="./imagenes/googleMaps.png"-->
 
 	</div> <!--Cierra contenedor1-->
 
