b7995c53ef30a12898e47501404cfe58ca400703
gustavowittbecker <32279987+gustavowittbecker@users.noreply.github.com>
2025-04-11 10:08:30 -0300

    ingles-español

--- index.php
+++ index.php
@@ -18,7 +18,10 @@
 	h1 {font-size:6em;text-align: center; }
 	h2 {font-size:3em;text-align: center;}
 	h3 {font-size:2em;text-align: left;}
+
+	
 	.fontGrande{font-size:22px} /*Para aplicar a un span por ejemplo*/
+	
 	img {	
 	    max-width: 100%; /*Ancho libre permitiendo al navegador calcularla en base o otros paramentros 
 		sin sobrepasar en ancho el tamaño de la caja*/
@@ -26,7 +29,7 @@
 	}
 
 
-	div.aparte{clear:both;height:25px;background-color:white}
+	div.aparte{clear:both;height:100px;background-color:white}
 
 	div.enlace{clear:both;height:20px;}
 	div.enlace a {font-size:2em}
@@ -41,7 +44,13 @@
 
 	div.bloque{display:block}
 </style>
+
 </Head>
+
+
+
+
+
 
 <body>
 	<header>
@@ -49,24 +58,15 @@
 	</header>
 	<div class="contenedorGlobal">
 		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<h2>Aprendé a bailar en serio</span></h2>
-		
 
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-
+		<hr />		
+		<h2 id="bailarEnSerio"></h2>		
 		<hr />
 
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
+
 		<div class="aparte"></div>
 		<main>
-		<h2>Aprendé <span class="fontGrande">a dominar tu cuerpo,<br/> a escuchar la música, 
-			</br>a seguirla <br/>y sobre todo <br/>a hacer sentir bien a tu pareja.</span></h2>
+		<h2 id="dominarCuerpo"></h2>
 		<div class="aparte"></div>
 		<div class="aparte"></div>
 		
@@ -83,17 +83,9 @@
 		</div>
 
 		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
 
-		<h2>Aprendé <span class="fontGrande">a pararte bien, <br/>
-							a caminar y a desplazarte correctamente,<br/>
-							a pivotear en giro con tus metatarsos y cadera,<br/>
-							a disociar el movimiento de tu torso.<br/>
-							Todo te ayudara a flexibilizar tu cuerpo.</span></h2>
+		<h2 id="pararteBien"></h2>
 
-		<div class="aparte"></div>
 		<div class="aparte"></div>
 
 		<div class="bloque">
@@ -109,15 +101,12 @@
 		</div>
 
 		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
 
-		<h2>Aprendé <span class="fontGrande"> a marcar un abrazo firme <br/>y a la vez flexible,<br/>
-		</span></h2>
+
+		<h2 id="abrazo"></h2>
 
 		<div class="aparte"></div>
-		<div class="aparte"></div>
+
 		
 		
 
@@ -135,15 +124,12 @@
 		</div>
 
 		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
 
-		<h2>Aprendé <span class="fontGrande"> a no memorizar figuras <br/>sino simplemente a usarlas sin pensar. <br/>
-		</span></h2>
+
+		<h2 id="memorizar"></h2>
 
 		<div class="aparte"></div>
-		<div class="aparte"></div>	
+	
 
 		<div class="bloque">
 			<div class="contenedorPorTercio">
@@ -158,18 +144,13 @@
 		</div>
 
 
-		<div class="aparte"></div>
-		<div class="aparte"></div>	
-		<div class="aparte"></div>
 		<div class="aparte"></div>	
 
 
-		<h2>Aprendé <span class="fontGrande">a usar ganchos y sacadas <br/>solo en los momentos adecuados.</span></h2>
+		<h2 id="ganchos"></h2>
 
 		<div class="aparte"></div>
-		<div class="aparte"></div>		
-		<div class="aparte"></div>
-		<div class="aparte"></div>
+
 		<div class="bloque">
 			<div class="contenedorPorTercio">
 				<img src="./imagenes/tangoGancho01.png" />
@@ -184,17 +165,10 @@
 
 
 		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
-		<div class="aparte"></div>
 
 
-		<h2>Aprendé <span class="fontGrande">a asociar el movimiento  <br/>a las ideas de <br/> agua, aire,
-	tierra y fuego</span></h2>
+		<h2 id="aft"></h2>
 
-<div class="aparte"></div>
-<div class="aparte"></div>		
-<div class="aparte"></div>
 <div class="aparte"></div>
 
 
@@ -212,19 +186,13 @@
 
 
 <div class="aparte"></div>
-<div class="aparte"></div>
-<div class="aparte"></div>
-<div class="aparte"></div>
 
 
 
-<h2>Salí a bailar<span class="fontGrande"> <br/>pensando que tu baile<br/> no es ni mejor ni peor que el de los demás. 
-			<br/>Es diferente, especial <br/>y tal vez único.</span></h2>
+<h2 id="sali"></h2>
 
 	<div class="aparte"></div>
-	<div class="aparte"></div>		
-	<div class="aparte"></div>
-	<div class="aparte"></div>
+
 
 
 	<div class="bloque">
@@ -249,18 +217,15 @@
 
 	<div class="aparte"></div>
 	<div class="aparte"></div>
-	<div class="aparte"></div>
-	<div class="aparte"></div>
-	<div class="aparte"></div>
-	<div class="aparte"></div>
+	
 
-
-	<h2>Aprender a bailar el tango <br/>en serio <span class="fontGrande"><br/>
-							es un camino de ida sin regreso.<br/>
-	</h2>
+	<hr/>
+	<h2 id="aprenderTango"></h2>
 
 	<hr/>
 </main>
+
+
 		<div style="display:flex;background-color:white;color:black">
 			<div class="contenedorPorTercio" >
 				<div class="contenedorPorTercio" >
@@ -288,10 +253,128 @@
 
 		<div class="aparte"></div>
 		<div class="aparte"></div>
-		<div class="aparte"></div>
 
 
+		
 	</div> <!--Cierra contenedorGlobal-->
 
+
+
 </body>
+
+<script>
+
+const es_bailarEnSerio = "Aprendé <span class='fontGrande'>a bailar en serio</span>";
+const en_bailarEnSerio = "Learn to <span class='fontGrande'> dance seriously </span>";
+
+const es_dominarCuerpo = "Aprendé <span class='fontGrande'>a dominar tu cuerpo, <br/>"+
+							"a escuchar la música, <br /> "+
+							"a seguirla  <br />"+
+							"y sobre todo <br />"+
+							"a hacer sentir bien a tu pareja. </span>";
+const en_dominarCuerpo = "Learn to <span class='fontGrande'> master your body, <br />"+ 
+						 "listen to the music, <br />"+
+						  "follow it, <br />"+ 
+						  "and above all, <br />"+
+						  "to make your partner feel good. </span>";
+
+const es_pararteBien = "Aprendé <span class='fontGrande'>a pararte bien, <br/>"+
+							"a caminar y a desplazarte correctamente,<br/>"+
+							"a pivotear en giro con tus metatarsos y cadera,<br/>"+
+							"a disociar el movimiento de tu torso.<br/>"+
+							"Todo te ayudara a flexibilizar tu cuerpo.</span>";
+const en_pararteBien = "Learn to  <span class='fontGrande'> stand up straight, <br />"+
+							"walk and move correctly, <br/>"+
+							"pivot in a turn with your metatarsals and hip<br/>"+
+							"dissociate the movement of your torso.<br/>"+
+							"Every thing will help you to make your body more flexible </span>";
+
+const es_abrazo = "Aprendé <span class='fontGrande'>a marcar un abrazo firme, <br />"+
+				"y ala vez flexible</span>";
+const en_abrazo = "Learn to <span class='fontGrande'>give a firm embrace<br />"+
+			"and flexible at the same time</span>";
+
+
+const es_memorizar = "Aprendé <span class='fontGrande'>a no memorizar figuras, <br/>"+
+			"sino simplemente usarlas.</span>";
+const en_memorizar = "Learn not to <span class='fontGrande'>memorize figures, <br/>"+
+			"but simply to use them without thinking.</span>";
+
+const es_ganchos = "Aprendé <span class='fontGrande'>a usar ganchos y sacadas, <br/>"+
+			"pero solo en los momentos adecuados.</span>";
+const en_ganchos = "Learn to <span class='fontGrande'>use ganchos and sacadas, <br/>"+
+			"but only at the right times</span>";
+
+const es_aft= "Aprendé <span class='fontGrande'>a asociar el movimiento a las ideas de <br />"+
+				"agua, aire, tierra y fuego. </span>";
+const en_aft= "Learn to <span class='fontGrande'>associate movement with the ideas of <br/>"+
+				"water, air, earth, and fire.</span>";
+
+			
+
+const es_sali = "Sali a bailar <span class='fontGrande'>"+
+					"pensando que tu baile <br/>"+	
+					"no es ni mejor ni peor que el de los demás. <br />"+
+					"Es diferente, especial y tal vez único.</span>";
+const en_sali = "Go away dancing <span class='fontGrande'> <br />"+
+					"with the understanding that your dance <br/>"+
+					"is neither better nor worse than anyone else's. <br/>"+
+					"It's different, special, and perhaps unique."+
+					"</span>";
+
+
+
+const es_aprenderTango = "Aprender a bailar tango en serio <br />"+
+							"<span class='fontGrande'>"+
+							"es un camino de ida sin regreso</span>";
+const en_aprenderTango = "Learn to dance Tango seriusly <br /><span class='fontGrande'>"+
+							"is a one-way street with no return.</span>";
+
+
+
+
+
+/*
+
+const es_ = "Aprendé <span class='fontGrande'>"+
+			"</span>";
+const en_ = "Learn not to <span class='fontGrande'>"+
+			"</span>";
+*/
+
+							
+
+let lenguaje = window.navigator.languages; //Es un array
+	
+
+/*document.getElementById("demo").innerHTML = lenguaje[0]; */
+
+alert(lenguaje[0]);
+
+if (lenguaje[0]=="es-ES") {
+	//alert("español");
+	document.getElementById("bailarEnSerio").innerHTML = es_bailarEnSerio;
+	document.getElementById("dominarCuerpo").innerHTML = es_dominarCuerpo;
+	document.getElementById("pararteBien").innerHTML = es_pararteBien;
+	document.getElementById("abrazo").innerHTML = es_abrazo;
+	document.getElementById("memorizar").innerHTML = es_memorizar;
+	document.getElementById("ganchos").innerHTML = es_ganchos;
+	document.getElementById("aft").innerHTML = es_aft;
+	document.getElementById("sali").innerHTML = es_sali;
+	document.getElementById("aprenderTango").innerHTML = es_aprenderTango;
+}
+else if (lenguaje[0]=="en") {
+	//alert("ingles");
+	document.getElementById("bailarEnSerio").innerHTML = en_bailarEnSerio;
+	document.getElementById("dominarCuerpo").innerHTML = en_dominarCuerpo;
+	document.getElementById("pararteBien").innerHTML = en_pararteBien;
+	document.getElementById("abrazo").innerHTML = en_abrazo;
+	document.getElementById("memorizar").innerHTML = en_memorizar;
+	document.getElementById("ganchos").innerHTML = en_ganchos;
+	document.getElementById("aft").innerHTML = en_aft;
+	document.getElementById("sali").innerHTML = en_sali;
+	document.getElementById("aprenderTango").innerHTML = en_aprenderTango;
+}
+
+</script>
 </html>
